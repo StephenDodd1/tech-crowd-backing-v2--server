ac5e390d5dd0430c448caedfcaa7bf8c42b332f9
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-13 17:02:39 -0500

    configured post endpoint for user, without id

--- src/content-router.js
+++ src/content-router.js
@@ -5,6 +5,8 @@ const contentRouter = express.Router();
 const jsonBodyParser = express.json();
 const POSTS = require('./test-posts.json');
 
+const users = [];
+
 contentRouter
    .route('/content/posts')
    .get((req,res) => {
@@ -19,4 +21,21 @@ contentRouter
       res.send(userName)
    })
 
+contentRouter
+   .route('/content/users')
+   .post(jsonBodyParser, (req,res) => {
+      console.log(req.body)
+      const { userName, password, fn, ln, dob, email } = req.body;
+      const newUser = {
+         userName,
+         password,
+         fn,
+         ln,
+         dob,
+         email
+      }
+      users.push(newUser);
+      console.log(users[0])
+      res.json(users)
+   })
 module.exports = contentRouter
