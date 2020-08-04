7f1ad810e20f6c2d18654b104aa293950eb576eb
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-04 16:42:17 -0500

    update

--- src/config.js
+++ src/config.js
@@ -8,6 +8,7 @@ module.exports = {
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://postgres@localhost/tech-crowd-backing-test-db",
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: 10000
 };
 //postgres://axobuouluiczdk:0d592e9e4454b71b39883e14dbe832020c6ee2a1b0911c5c45de8cfd770c178f@ec2-34-225-162-157.compute-1.amazonaws.com:5432/d43no5c5i8qk6b
