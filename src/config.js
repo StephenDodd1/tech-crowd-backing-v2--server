9f2395152bd7ba5736e1550609035e075a8c3bbb
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-08 18:27:55 -0500

    update client origin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://tech-crowd-backing-v2-client.vercel.app",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://tech-crowd-backing-v2-client.vercel.app" || "https://techcrowdbacking.com",
   DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgresql://postgres@localhost/tech-crowd-backing-db", 
